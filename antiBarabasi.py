ebfa6da28170ef34851354a9e480135efdf89dca
Han Zhang <zhanghan426@gmail.com>
2013-01-18 13:01:04 +0800

    1.0.0

--- antiBarabasi.py
+++ antiBarabasi.py
@@ -5,48 +5,37 @@ import networkx as nx
 from networkx.generators.classic import empty_graph, path_graph, complete_graph
 
 from collections import defaultdict
-def barabasi_albert_graph(n, m, seed=None):
-        
-    if m < 1 or  m >=n:
-        raise nx.NetworkXError(\
-              "Barabási-Albert network must have m>=1 and m<n, m=%d,n=%d"%(m,n))
-    if seed is not None:
-        random.seed(seed)    
+def prob_subset(G,m):
+    result = set()
+    choices = [(x,1.0/G.degree(x)) for x in G.nodes()]
+    total = sum(w for c,w in choices)
 
-    # Add m initial nodes (m0 in barabasi-speak) 
-    G=empty_graph(m)
-    G.name="barabasi_albert_graph(%s,%s)"%(n,m)
-    # Target nodes for new edges
-    targets=list(range(m))
-    # List of existing nodes, with nodes repeated once for each adjacent edge 
-    repeated_nodes=[]     
-    # Start adding the other n-m nodes. The first node is m.
-    source=m 
-    while source<n: 
-        # Add edges to m nodes from the source.
-        G.add_edges_from(zip([source]*m,targets)) #tuple (new node, old node)
-        # Add one node to the list for each new edge just created.
-        repeated_nodes.extend(targets)
-        # And the new node "source" has m edges to add to the list.
-        repeated_nodes.extend([source]*m) 
-        # Now choose m unique nodes from the existing nodes 
-        # Pick uniformly from repeated_nodes (preferential attachement) 
-        targets = _random_subset(repeated_nodes,m)
-        source += 1
+    upto = 0
+    while len(result)<m:
+        for c,w in choices:
+            if upto+w >total:
+                result.add(c)
+            upto += w
+
+    return list(result)
+
+
+def anti_barabasi(n,m):
+    G = complete_graph(m)
+    #target nodes for new nodes
+    targets = list(range(m))
+    new = m#initialize = m
+    while new < n:
+        G.add_edges_from(zip([new]*m,targets))#add m edge together
+        targets = prob_subset(G,m)
+        new += 1
     return G
-def _random_subset(seq,m):
-    """ Return m unique elements from seq.
 
-    This differs from random.sample which can return repeated
-    elements if seq holds repeated elements.
-    """
-    targets=set()
-    while len(targets)<m:
-        x=random.choice(seq)
-        targets.add(x)
-    return targets
+
     
 def main():
-    pass
-    if __name__ == '__main__':
+    g = anti_barabasi(20,5)
+    print g.nodes()
+    print g.edges()    
+if __name__ == '__main__':
         main()
